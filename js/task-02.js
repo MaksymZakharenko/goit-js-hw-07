6cce50b26e1cab6edad32e3e5a02d92dbedebe9f
MaksymZakharenko <raspik83@gmail.com>
2021-04-28 22:03:18 +0300

    1-3 задача выполнена

---
+++ js/task-02.js
@@ -0,0 +1,23 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const itemRef = document.querySelector('#ingredients')
+
+
+const massiv = ingredients.map(elem => {
+const liItem = document.createElement('li');
+  liItem.textContent = elem;
+
+  return liItem;
+})
+
+itemRef.append(...massiv);
+
+console.log(itemRef);
+
